3ab93542e323ee0f72e23ff0c2965af2950bff53
sekayasin <sekayasin@gmail.com>
2022-04-16 22:50:01 -0500

    project initial setup

---
+++ src/test/java/edu/miu/shopmartbackend/ShopmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.shopmartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
